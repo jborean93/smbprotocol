03071c983b26dc83e35c4ba1a551b72634744dc7
David A. Hannasch <David.A.Hannasch@gmail.com>
2021-04-26 20:22:03 -0600

    replace ntpath.join(src with join_local_or_remote_path(src

--- smbclient/shutil.py
+++ smbclient/shutil.py
@@ -331,7 +331,7 @@ def copytree(src, dst, symlinks=False, ignore=None, copy_function=copy2, ignore_
         if dir_entry.name in ignored:
             continue
 
-        src_path = ntpath.join(src, dir_entry.name)
+        src_path = join_local_or_remote_path(src, dir_entry.name)
         dst_path = join_local_or_remote_path(dst, dir_entry.name)
 
         try:
