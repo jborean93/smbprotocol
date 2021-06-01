226bb97ab124fb1d67929d8caf54786afbac1bc7
Alex-Weatherhead <22604743+Alex-Weatherhead@users.noreply.github.com>
2021-06-01 13:25:13 -0400

    Ignore ENOTCONN socket errors

--- smbprotocol/transport.py
+++ smbprotocol/transport.py
@@ -86,7 +86,11 @@ class Tcp(object):
 
                 # Sending shutdown first will tell the recv thread (for both select and recv) that the socket has data
                 # which returns b'' meaning it was closed.
-                self._sock.shutdown(socket.SHUT_RDWR)
+                try:
+                    self._sock.shutdown(socket.SHUT_RDWR)
+                except socket.error as e:
+                    if e.errno != errno.ENOTCONN:
+                        raise  # pragma: no cover
 
                 # This is even more special, we cannot close the socket if we are in the middle of a select or recv().
                 # Doing so causes either a timeout (bad!) or bad fd descriptor (somewhat bad). By shutting down the
