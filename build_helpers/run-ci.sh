fb3a960752379701054b7e195fcb4c272b8c2a4d
Maxim Martynov <martinov_m_s_@mail.ru>
2023-08-19 13:25:30 +0300

    Combine coverage before upload

--- build_helpers/run-ci.sh
+++ build_helpers/run-ci.sh
@@ -2,12 +2,38 @@
 
 # Set by GHA setup-python
 if [[ -n "${pythonLocation}" ]]; then
-    PATH="${pythonLocation}/bin:${PATH}"
+  PATH="${pythonLocation}/bin:${PATH}"
 fi
 
 source ./build_helpers/lib.sh
 
-lib::setup::smb_server
-lib::setup::python_requirements
-lib::sanity::run
-lib::tests::run
+if [[ "$#" == 0 ]]; then
+  set "$@" requirements lint coverage-erase start-server tests stop-server coverage-combine
+fi
+
+while [[ "$#" > 0 ]]; do
+  case "$1" in
+  "requirements")
+    lib::requirements::install
+    ;;
+  "lint")
+    lib::sanity::run
+    ;;
+  "tests")
+    lib::tests::run
+    ;;
+  "start-server")
+    lib::server::start
+    ;;
+  "stop-server")
+    lib::server::stop
+    ;;
+  "erase-coverage")
+    lib::coverage::erase
+    ;;
+  "combine-coverage")
+    lib::coverage::combine
+    ;;
+  esac
+  shift
+done
