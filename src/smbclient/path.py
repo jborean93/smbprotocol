174a3de5c9f607b79f63d8cc4dc22ad59a174544
Samuel J. Woodward <sam@woodward.fyi>
2026-04-01 15:48:12 -0400

    Cleanup path for PR

--- src/smbclient/path.py
+++ src/smbclient/path.py
@@ -1,8 +1,6 @@
 # Copyright: (c) 2019, Jordan Borean (@jborean93) <jborean93@gmail.com>
 # MIT License (see LICENSE or https://opensource.org/licenses/MIT)
 
-import ntpath
-
 import errno
 import stat as py_stat
 
